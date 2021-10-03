b5c374e86eb6ba1f4576fb5c4046798466a4cea6
how <st010447@gmail.com>
2021-10-03 21:13:55 +0800

    append

---
+++ Linked-List/Linked-List.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class ListNode
+{
+public:
+    int data;
+    ListNode *next; //C++ null=0
+};
+class LinkedList
+{
+public:
+    void append(ListNode **head, int data);
+    {
+    }
+    // void insert(ListNode node, int index)
+    // {
+    // }
+}
+
+main()
+{
+    ListNode *head = new ListNode();
+}
