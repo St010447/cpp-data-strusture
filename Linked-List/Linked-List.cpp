31303e2daf8ff83cb77fbc57ac2cea3f41c1b06b
how <st010447@gmail.com>
2021-10-03 23:08:04 +0800

    append node

--- Linked-List/Linked-List.cpp
+++ Linked-List/Linked-List.cpp
@@ -6,12 +6,18 @@ class ListNode
 public:
     int data;
     ListNode *next; //C++ null=0
+    ListNode() : data(0), next(0){};
 };
 class LinkedList
 {
 public:
-    void append(ListNode **head, int data);
+    void append(ListNode *current, int data)
     {
+        ListNode *new_node = new ListNode();
+        new_node->data = data;
+        for (; current->next != 0; current = current->next)
+            ;
+        current->next = new_node;
     }
     // void insert(ListNode node, int index)
     // {
@@ -21,4 +27,5 @@ public:
 main()
 {
     ListNode *head = new ListNode();
+    LinkedList A;
 }
