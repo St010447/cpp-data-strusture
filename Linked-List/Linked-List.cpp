1236e3dcf959acee5c9cac760b97551841f9c2b8
oh <st010447@gmail.com>
2021-10-04 14:34:06 +0800

    append done

--- Linked-List/Linked-List.cpp
+++ Linked-List/Linked-List.cpp
@@ -7,25 +7,44 @@ public:
     int data;
     ListNode *next; //C++ null=0
     ListNode() : data(0), next(0){};
+    ListNode(int a) : data(a), next(0){};
 };
 class LinkedList
 {
 public:
-    void append(ListNode *current, int data)
+    ListNode *head = new ListNode();
+    ListNode *current = new ListNode();
+    void append(int input)
     {
         ListNode *new_node = new ListNode();
-        new_node->data = data;
+        current = head;
+        new_node->data = input;
         for (; current->next != 0; current = current->next)
-            ;
+            cout << current->next;
         current->next = new_node;
     }
-    // void insert(ListNode node, int index)
-    // {
-    // }
-}
+    void print_data()
+    {
+        if (head->next == 0)
+        {
+            cout << "list is empty" << endl;
+        }
+        else
+        {
+            current = head;
+            while (current->next != 0)
+            {
+                current = current->next;
+                cout << current->data << "->";
+            }
+            cout << 'Null';
+        }
+    }
+};
 
 main()
 {
-    ListNode *head = new ListNode();
-    LinkedList A;
+    LinkedList *A = new LinkedList();
+    A->append(20);
+    A->print_data();
 }
